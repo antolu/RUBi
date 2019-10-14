227aa311780452ffbf97b573c9e48bd340d967a1
Anton Lu <antolu@kth.se>
2019-10-14 11:55:42 +0000

    Edit argparse dependency in main, add documentation to split script and made executable

--- tools/parse_visual_features.py
+++ tools/parse_visual_features.py
@@ -1,3 +1,5 @@
+#!//usr/bin/env python2
+
 import argparse
 import os
 import pickle
@@ -6,11 +8,35 @@ import base64
 import csv
 import sys
 
+"""
+This file splits a pretrained Faster R-CNN features archive .tsv into 
+individual files for each COCO image id. the split files will be placed
+in the same directory as the original .tsv file. 
+
+The script has to be run with python 2, and for use with the shipped
+Tensorflow/Caffe containers it is run simply as
+
+```shell
+./tools/parse_visual_features.py [path/to/tsvfile]
+```
+"""
+
 
 FIELDNAMES = ['image_id', 'image_w','image_h','num_boxes', 'boxes', 'features']
 csv.field_size_limit(sys.maxsize)
 
 def split_tsv(infile):
+    """
+    Function to split the infile .tsv into individial files for each COCO
+    image id, one pickled .pkl file containing the information of the image
+    as a dict, and one .npz file containing the extracted features tensor
+    as numpy array as well as the feature boxes.
+
+    Parameters:
+    -----------
+    infile: str
+        Path to the file to split
+    """
     with open(infile, "r+b") as tsv_in_file:
         filepath = os.path.split(infile)[0]
 
@@ -33,7 +59,7 @@ def split_tsv(infile):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--tsvfile", required=True, help="The path to the TSV file to split")
+    parser.add_argument("tsvfile", help="The path to the TSV file to split")
 
     args = parser.parse_args()
 
