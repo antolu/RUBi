e0d7655ef80b6f5fec0cf9e03c742d670991a65a
Matheus <matheusfranca.t19@gmail.com>
2019-10-17 15:32:37 +0000

    add answer type

--- dataloader.py
+++ dataloader.py
@@ -33,14 +33,15 @@ class DataLoaderVQA(data.Dataset):
         self.coco_val_path = coco_val_path
         self.trainval_features_path = trainval_features_path
         self.test_features_path = test_features_path
-        self.answer_type = args_dic.answer_type # list : ['yes/no', 'number', 'other']
-        self.dataset = args_dic.dataset  # vqacp_v2 | vqa_v2
-        # self.dataset = 'vqa_v2'
-        # self.answer_type = ['number']
+        self.answer_type = args_dict.answer_type # list : ['yes/no', 'number', 'other']
+        self.dataset = args_dict.dataset  # vqacp_v2 | vqa_v2
 
         # only use the top 3000 answers
         df_annot = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_train_annotations.json'))
         top_3000_answer = list(df_annot['multiple_choice_answer'].value_counts().index)[:3000]
+        self.answer2idx = {}
+        for idx, answer in enumerate(top_3000_answer):
+            self.answer2idx[answer] = idx
     
         # choose train or test dataset
         if self.dataset == 'vqacp_v2':
@@ -67,11 +68,7 @@ class DataLoaderVQA(data.Dataset):
                 
             df_annot = pd.DataFrame(df_annot['annotations'])
             df_quest = pd.DataFrame(df_quest['questions'])
-                
 
-        #df = pd.merge(df_annot[['question_type', 'multiple_choice_answer',
-        #                        'image_id', 'answer_type', 'question_id']]
-        #              , df_quest[['coco_split', 'question', 'question_id']], on='question_id')
         df = pd.merge(df_annot[['multiple_choice_answer',
                                 'image_id', 'answer_type', 'question_id']]
                       , df_quest[['question', 'question_id']], on='question_id')
@@ -79,14 +76,30 @@ class DataLoaderVQA(data.Dataset):
         df = df[(df['multiple_choice_answer'].isin(top_3000_answer)) & 
                 (df['answer_type'].isin(self.answer_type))]
         
-
-        #self.images_path = df.apply(lambda x: self.get_img_path(x), axis=1)
         self.questions = df['question']
         self.answers = df['answer_type']
         self.img_embeddings_path = df['image_id'].apply(lambda x: self.get_visual_features_path(x))
 
     def __len__(self):
         return len(self.questions)
+    
+    def get_img_path(self, row):
+        """
+        Returns the right path based on the question_id and coco_split
+
+        Parameters
+        ----------
+        row : row of DataFrame
+            row with all atributes of que dataset
+
+        Returns
+        -------
+        path to the corresponding image
+        """
+        img_id = str(row['image_id'])
+        img_folder = row['coco_split']
+        full_number = ''.join((12 - len(img_id)) * ['0']) + img_id
+        return os.path.join(self.dir_data, img_folder, "COCO_" + img_folder + "_" + full_number + ".jpg")
     
     def get_visual_features_path(self, image_id):
         """
@@ -180,12 +193,14 @@ class DataLoaderVQA(data.Dataset):
 
         # Answer
         answer = self.preprocess_sentence(self.answers.iloc[index])
-        
+        answer_one_hot = torch.zeros(3000)
+        answer_one_hot[self.answer2idx[answer]] = 1.0
         item = {
             'img_embed': img_embedding,
             #'image': image,
             'question': question,
-            'answer': answer
+            'answer': answer,
+            'answer_one_hot': answer_one_hot
         }
 
         return item
