a55d0e25fc8c98afd8fd4c4cf077c97c35bba8cc
MatheusFranca <matheusfranca.t19@gmail.com>
2019-10-17 22:41:32 +0200

    bug fix

--- dataloader.py
+++ dataloader.py
@@ -10,9 +10,24 @@ import torchvision.transforms as transforms
 
 from PIL import Image
 
+
+
+import os
+import pickle
+import string
+import json
+import numpy as np
+import torch
+import torch.utils.data as data
+import pandas as pd
+import torchvision.transforms as transforms
+
+from PIL import Image
+
 class DataLoaderVQA(data.Dataset):
     def __init__(self, 
                  args_dict,
+                 set,
                  dir_data='data',
                  coco_train_path="data/train2014",
                  coco_val_path="data/val2014",
@@ -26,63 +41,80 @@ class DataLoaderVQA(data.Dataset):
         """
 
         self.args_dict = args_dict
+        self.set = set
         self.dir_data = dir_data
         self.coco_train_path = coco_train_path
         self.coco_val_path = coco_val_path
         self.trainval_features_path = trainval_features_path
         self.test_features_path = test_features_path
+        self.answer_type = args_dict.answer_type # list : ['yes/no', 'number', 'other']
         self.dataset = args_dict.dataset  # vqacp_v2 | vqa_v2
-
-        if args_dict.answer_type == 'all':
-            self.answer_type = ['yes/no', 'number', 'other']
-        else:
-            self.answer_type = [args_dict.answer_type]
 
         # only use the top 3000 answers
         df_annot = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_train_annotations.json'))
         top_3000_answer = list(df_annot['multiple_choice_answer'].value_counts().index)[:3000]
-        self.answ
+        self.answer2idx = {}
+        for idx, answer in enumerate(top_3000_answer):
+            self.answer2idx[answer] = idx
     
         # choose train or test dataset
         if self.dataset == 'vqacp_v2':
-            if args_dict.train:
+            if self.set == 'train':
                 df_annot = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_train_annotations.json'))
                 df_quest = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_train_questions.json'))
 
-            elif args_dict.test:
+            elif self.set == 'test':
                 df_annot = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_test_annotations.json'))
                 df_quest = pd.read_json(os.path.join(self.dir_data, 'vqacp_v2', 'vqacp_v2_test_questions.json'))
                 
         elif self.dataset == 'vqa_v2':
-            if args_dict.train:
+            if self.set == 'train':
                 df_annot = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_mscoco_train2014_annotations.json')))
                 df_quest = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_OpenEnded_mscoco_train2014_questions.json')))
 
-            elif args_dict.test:
+            elif self.set == 'test':
                 df_annot = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_mscoco_val2014_annotations.json')))
                 df_quest = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_OpenEnded_mscoco_val2014_questions.json')))
 
-            elif args_dict.train.test_dev:
+            elif self.set == 'test-dev':
                 df_annot = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_mscoco_val2014_annotations.json')))
                 df_quest = json.load(open(os.path.join(self.dir_data, 'vqa_v2', 'v2_OpenEnded_mscoco_test-dev2015_questions.json')))
                 
             df_annot = pd.DataFrame(df_annot['annotations'])
             df_quest = pd.DataFrame(df_quest['questions'])
 
-
-        df = pd.merge(df_annot[['multiple_choice_answer',
-                                'image_id', 'answer_type', 'question_id']]
-                      , df_quest[['question', 'question_id']], on='question_id')
+        df = pd.merge(df_annot[['question_type', 'multiple_choice_answer',
+                                'image_id', 'answer_type', 'question_id']], 
+				df_quest[['coco_split', 'question', 'question_id']], on='question_id')
         
         df = df[(df['multiple_choice_answer'].isin(top_3000_answer)) & 
                 (df['answer_type'].isin(self.answer_type))]
-
+        
+	#self.images_path = df.apply(lambda x: self.get_img_path(x), axis=1)
         self.questions = df['question']
         self.answers = df['answer_type']
         self.img_embeddings_path = df['image_id'].apply(lambda x: self.get_visual_features_path(x))
 
     def __len__(self):
         return len(self.questions)
+    
+    def get_img_path(self, row):
+        """
+        Returns the right path based on the question_id and coco_split
+
+        Parameters
+        ----------
+        row : row of DataFrame
+            row with all atributes of que dataset
+
+        Returns
+        -------
+        path to the corresponding image
+        """
+        img_id = str(row['image_id'])
+        img_folder = row['coco_split']
+        full_number = ''.join((12 - len(img_id)) * ['0']) + img_id
+        return os.path.join(self.dir_data, img_folder, "COCO_" + img_folder + "_" + full_number + ".jpg")
     
     def get_visual_features_path(self, image_id):
         """
@@ -155,8 +187,8 @@ class DataLoaderVQA(data.Dataset):
         -------
         question preprocessed
         """
-        prep_quest = sentence.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
-        prep_quest = sentence.lower()  # lower case
+        prep_quest = sentence.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+        prep_quest = sentence.lower() # lower case
     
         return prep_quest
 
@@ -176,12 +208,17 @@ class DataLoaderVQA(data.Dataset):
 
         # Answer
         answer = self.preprocess_sentence(self.answers.iloc[index])
-        
+        answer_one_hot = torch.zeros(3000)
+        answer_one_hot[self.answer2idx[answer]] = 1.0
         item = {
             'img_embed': img_embedding,
             #'image': image,
             'question': question,
-            'answer': answer
+            'answer': answer,
+            'answer_one_hot': answer_one_hot
         }
 
         return item
+
+
+
