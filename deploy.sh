b7a32b271aa5128b7f02da207da4710bc860051a
Anton Lu <antolu@kth.se>
2019-10-22 12:42:36 +0000

    fix cuda install on uuntu

--- deploy.sh
+++ deploy.sh
@@ -81,7 +81,19 @@ installPackages() {
         rm -rf libnvidia-container nvidia-container-toolkit
         ;;
         ubuntu|debian)
-        sudo apt install -y nvidia-cuda-toolkit nvidia-container-toolkit
+	    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+	    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+	    sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+
+	    curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+		sudo apt-key add -
+	    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+	    curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+		sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+
+	    sudo apt-get update
+	    sudo apt-get -y install cuda nvidia-container-runtime
         ;;
     esac
     sudo systemctl restart docker
